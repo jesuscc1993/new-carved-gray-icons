30bcfd0681f685c7bad331e73a5e824d42601c0d
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2022-05-07 17:43:26 +0200

    Updated preview with all resolutions.

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,5 @@
+const resolutions = [256, 48, 32, 24, 16];
+
 const fetchJson = (url) => {
   return fetch(url).then((response) => response.json());
 };
@@ -6,15 +8,23 @@ const initialize = () => {
   const output = jQuery('#output');
 
   fetchJson('files.json').then(({ files }) => {
-    const filesGrid = jQuery(`<div class="grid"></div>`);
+    resolutions.forEach((resolution) => {
+      const groupElement = jQuery(`
+        <div class="group">
+          <h2>${resolution}px</h2>
+        </div>
+      `);
+      const filesGrid = jQuery(`<div class="grid"></div>`);
 
-    files.forEach((file) => {
-      filesGrid.append(
-        `<img class="icon" src="../PNG/${file}_256px.png" title="${file}">`
-      );
+      files.forEach((file) => {
+        filesGrid.append(
+          `<img class="icon" src="../PNG/${file}_${resolution}px.png" title="${file}">`
+        );
+      });
+
+      groupElement.append(filesGrid);
+      output.append(groupElement);
     });
-
-    output.append(filesGrid);
   });
 };
 
