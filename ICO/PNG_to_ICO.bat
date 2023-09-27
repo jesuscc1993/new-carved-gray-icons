3073b800d9436df6aa7df72c9481da2bf6587399
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-09-27 19:10:53 +0200

    Added magnet icon.

--- ICO/PNG_to_ICO.bat
+++ ICO/PNG_to_ICO.bat
@@ -31,6 +31,7 @@ for %%x in (
   image
   library
   lock
+  magnet
   magnifier
   shortcuts
   star
@@ -45,5 +46,5 @@ for %%x in (
   windows
 ) do (
   REM absolute path is more performant
-  "Z:\Software\Heavy\Multimedia\ImageMagick\magick.exe" convert "../PNG/%%x_16px.png" "../PNG/%%x_24px.png" "../PNG/%%x_32px.png" "../PNG/%%x_48px.png" "../PNG/%%x_256px.png" "%%x.ico"
+  "Y:\Software\Heavy\Multimedia\ImageMagick\magick.exe" convert "../PNG/%%x_16px.png" "../PNG/%%x_24px.png" "../PNG/%%x_32px.png" "../PNG/%%x_48px.png" "../PNG/%%x_256px.png" "%%x.ico"
 )
--- demo/index.js
+++ demo/index.js
@@ -7,24 +7,22 @@ const fetchJson = (url) => {
 const initialize = () => {
   const output = jQuery('#output');
 
-  fetchJson('files.json').then(({ files }) => {
-    resolutions.forEach((resolution) => {
-      const groupElement = jQuery(`
-        <div class="group">
-          <h2>${resolution}px</h2>
-        </div>
-      `);
-      const filesGrid = jQuery(`<div class="grid"></div>`);
+  resolutions.forEach((resolution) => {
+    const groupElement = jQuery(`
+      <div class="group">
+        <h2>${resolution}px</h2>
+      </div>
+    `);
+    const filesGrid = jQuery(`<div class="grid"></div>`);
 
-      files.forEach((file) => {
-        filesGrid.append(
-          `<img class="icon" src="../PNG/${file}_${resolution}px.png" title="${file}">`
-        );
-      });
-
-      groupElement.append(filesGrid);
-      output.append(groupElement);
+    files.forEach((file) => {
+      filesGrid.append(
+        `<img class="icon" src="../PNG/${file}_${resolution}px.png" title="${file}">`
+      );
     });
+
+    groupElement.append(filesGrid);
+    output.append(groupElement);
   });
 };
 
