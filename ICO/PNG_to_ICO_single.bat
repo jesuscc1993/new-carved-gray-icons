f4a549887a9d32dfb0d99773c87fba3d30e31cfe
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-11-01 15:59:05 +0100

    Updated adult icon.

--- ICO/PNG_to_ICO_single.bat
+++ ICO/PNG_to_ICO_single.bat
@@ -2,4 +2,5 @@
 
 set /p x="Enter the name of the file to convert (extension excluded): "
 "Y:\Software\Heavy\Multimedia\ImageMagick\magick.exe" convert "../PNG/%x%_16px.png" "../PNG/%x%_24px.png" "../PNG/%x%_32px.png" "../PNG/%x%_48px.png" "../PNG/%x%_256px.png" "%x%.ico"
-set /p x="Successfully generated %x%.ico. Press enter to exit."
+echo Successfully generated %x%.ico
+set /p x="Press enter to exit."
