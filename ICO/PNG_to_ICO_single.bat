5788f84a2e07080ab312e816c36ced4ad3c895be
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-10-23 21:33:25 +0200

    Updated PY icon.

--- ICO/PNG_to_ICO_single.bat
+++ ICO/PNG_to_ICO_single.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set /p x="Enter the name of the file to convert (extension excluded): "
-"Y:\Software\Heavy\Multimedia\ImageMagick\magick.exe" convert "../PNG/%x%_16px.png" "../PNG/%x%_24px.png" "../PNG/%x%_32px.png" "../PNG/%x%_48px.png" "../PNG/%x%_256px.png" "%x%.ico"
+magick.exe convert "../PNG/%x%_16px.png" "../PNG/%x%_24px.png" "../PNG/%x%_32px.png" "../PNG/%x%_48px.png" "../PNG/%x%_256px.png" "%x%.ico"
 echo Successfully generated %x%.ico
 set /p x="Press enter to exit."
